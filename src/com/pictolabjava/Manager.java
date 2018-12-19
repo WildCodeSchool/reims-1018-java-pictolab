45cf60427ab1809418d3c6fc960e4a639cbcd519
marthe.charpy <marthe.charpy@gmail.com>
2018-12-19 17:26:36 +0100

    creation Manager.java

---
+++ src/com/pictolabjava/Manager.java
@@ -0,0 +1,25 @@
+package com.pictolabjava;
+
+import java.util.ArrayList;
+
+import com.beans.Activite;
+import com.beans.Tag;
+
+public class Manager {
+	private ArrayList<Activite> activiteArray;
+	private ArrayList<Tag> tagArray;
+
+	
+	public Manager(){
+	
+	this.activiteArray= new ArrayList<Activite>();
+	this.tagArray= new ArrayList<Tag>();
+	this.activiteArray.add(new Activite(1, "Fablab", "orem Elsass ipsum vielmols, und baeckeoffe", "orem Elsass ipsum vielmols, und baeckeoffe morbi varius lacus mamsell schneck rhoncus aliquam elementum Carola id, leo dolor kuglopf Heineken ac leo auctor, kartoffelsalad Chulien Christkindelsmärik yeuh. sagittis libero. vulputate turpis, knepfle Mauris amet s'guelt météor non DNA, placerat nullam sed schpeck Gal.", 
+			"mardi et jeudi"));
+	}
+	
+}
+		
+	
+	
+
