3d855ee2d1132fa96aba4961ca48ab4938bd3d1e
Justine Dubois <justine.dubois.pro@gmail.com>
2018-12-20 17:21:16 +0100

    recuperation des tags par url

--- src/com/pictolab/beans/Tag.java
+++ src/com/pictolab/beans/Tag.java
@@ -1,58 +1,39 @@
 package com.pictolab.beans;
 
-import java.util.ArrayList;
-
 public class Tag {
 	
 	private int id;
 	private String title;
 	private String urlName;
-	private ArrayList<Integer> activities;
 
 	
-	public Tag(int id, String title, String urlName, ArrayList<Integer> activities) {
+	public Tag(int id, String title, String urlName) {
 		this.id=id;
 		this.title=title;
 		this.urlName=urlName;
-		this.activities=activities;
 	}
 
 	public int getId() {
 		return id;
 	}
 
-
 	public String getTitle() {
 		return title;
 	}
-
 
 	public String getUrlName() {
 		return urlName;
 	}
 
-
-	public ArrayList<Integer> getActivities() {
-		return activities;
-	}
-
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
-
 	public void setUrlName(String urlName) {
 		this.urlName = urlName;
-	}
-
-
-	public void setActivities(ArrayList<Integer> activities) {
-		this.activities = activities;
 	}
 }
