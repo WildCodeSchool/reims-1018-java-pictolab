ef6f98e80596a08f411d7efe7fc8dad9d5cb7bde
Justine Dubois <justine.dubois.pro@gmail.com>
2018-12-20 11:41:27 +0100

    creation arraylist de tags et methode getAllTags dans TagManager

--- src/com/pictolab/models/TagManager.java
+++ src/com/pictolab/models/TagManager.java
@@ -5,12 +5,23 @@ import java.util.ArrayList;
 import com.pictolab.beans.Tag;
 
 public class TagManager {
-	private ArrayList<Tag> tagArray;
+	private ArrayList<Integer> activities;
+	private ArrayList<Tag> tags;
 
 	public TagManager() {
-		this.tagArray= new ArrayList<Tag>();
+		this.tags = new ArrayList<Tag>();
+		this.activities = new ArrayList<Integer>();
+		
+		for(int i=1; i<=3; i++) {
+			this.activities.add(i);
+		}
+
+		for(int j = 1; j <= 17; j++) {
+			this.tags.add(new Tag(j, "Manuel", "activite_manuelle", activities));			
+		}
 	}
-
 	
-
+	public ArrayList<Tag> getAllTags() {
+		return this.tags;
+	}
 }
