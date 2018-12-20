b5344419d21ec33c5a7833133d2ff567e5e747d9
Justine Dubois <justine.dubois.pro@gmail.com>
2018-12-20 11:53:22 +0100

    appel de getAllTags dans la servlet et affichage des tags grace a cette methode (jsp)

--- src/com/pictolab/controllers/IndexServlet.java
+++ src/com/pictolab/controllers/IndexServlet.java
@@ -7,26 +7,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.pictolab.models.TagManager;
+
 /**
  * Servlet implementation class IndexServlet
  */
 @WebServlet("/index.html")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private TagManager tagManager;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
     public IndexServlet() {
         super();
-        // TODO Auto-generated constructor stub
+        tagManager = new TagManager();
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		request.setAttribute("tags", this.tagManager.getAllTags());
+		
 		this.getServletContext().getRequestDispatcher("/WEB-INF/index.jsp").forward(request,response);
 	}
 
